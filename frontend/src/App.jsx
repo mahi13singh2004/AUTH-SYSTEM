2772a0502dd8123ab03787a62f3dc1ce6440a682
mahi13singh2004 <mahi13singh2004@gmail.com>
2025-06-27 09:47:34 +0530

    added error handling in home pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,13 +8,17 @@ import RedirectRoute from './components/RedirectRoute.jsx'
 import ProtectedRoute from './components/ProtectedRoute.jsx'
 import { useAuthStore } from './store/auth.store.js'
 import Navbar from './components/Navbar.jsx'
+import { useLocation } from 'react-router-dom'
 
 const App = () => {
+  const location=useLocation()
   const { checkAuth } = useAuthStore()
 
   useEffect(() => {
-    checkAuth()
-  }, []) 
+    if (!["/login", "/signup"].includes(location.pathname)) {
+      checkAuth()
+    }
+  }, [location.pathname]) 
 
   return (
     <>
